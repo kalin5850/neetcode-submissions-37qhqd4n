21e24f840f491e5a69778ec2663e99ca14001215
Kai <kalin5850@gmail.com>
2026-08-05 10:13:12 -0400

    Add: unboundedKnapsack - submission-3

---
+++ Data Structures & Algorithms/unboundedKnapsack/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumProfit(self, profit: List[int], weight: List[int], capacity: int) -> int:
+
+        def dp() -> int:
+            dp = [[0] * (capacity + 1) for _ in range(len(profit) + 1)]
+            for i in range(1, len(profit) + 1):
+                for w in range(1, capacity + 1):
+                    if w - weight[i - 1] >= 0:
+                        dp[i][w] = max(
+                            dp[i - 1][w], profit[i - 1] + dp[i][w - weight[i - 1]]
+                        )
+                    else:
+                        dp[i][w] = dp[i - 1][w]
+            return dp[len(profit)][capacity]
+        
+        return dp()
